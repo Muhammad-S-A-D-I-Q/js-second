cd68fc1953ee0713cf3c36f1e8bdfb24e4a0bf9b
Muhammadsadiq2611 <115625441+Muhammadsadiq2611@users.noreply.github.com>
2023-07-26 23:02:28 +0500

    sec assssent !

---
+++ chapter12-13/chapter12-13.js
@@ -0,0 +1,158 @@
+document.write("<h1>Chapter 12-13</h1>");
+
+
+
+
+
+
+//
+
+
+
+
+document.write("<h2>ASCII Character's...</h2>");
+
+var ASCII = parseInt(prompt("Enter ASCII characters to convert in alphabets "));
+
+if (ASCII === 65) {
+   document.write("A");
+}else if (ASCII === 66 ){
+     document.write("B");
+}else if (ASCII === 67 ){
+    document.write("C");
+}else if (ASCII === 68 ){
+    document.write("D");
+}else if (ASCII === 69 ){
+    document.write("E");
+}else if (ASCII === 70 ){
+    document.write("F");
+}else if (ASCII === 71 ){
+    document.write("G");
+}else if (ASCII === 72 ){
+    document.write("H");
+}else if (ASCII === 73 ){
+    document.write("I");
+}else if (ASCII === 74 ){
+    document.write("J");
+}else if (ASCII === 75 ){
+    document.write("K");
+}else if (ASCII === 76 ){
+    document.write("L");
+}else if (ASCII === 77 ){
+    document.write("M");
+}else if (ASCII === 78 ){
+    document.write("N");
+}else if (ASCII === 79 ){
+    document.write("O");
+}else if (ASCII === 80 ){
+    document.write("P");
+}else if (ASCII === 81 ){
+    document.write("Q");
+}else if (ASCII === 82 ){
+    document.write("R");
+}else if (ASCII === 83 ){
+    document.write("S");
+}else if (ASCII === 84 ){
+    document.write("T");
+}else if (ASCII === 85 ){
+    document.write("U");
+}else if (ASCII === 86 ){
+    document.write("V");
+}else if (ASCII === 87 ){
+    document.write("W");
+}else if (ASCII === 85 ){
+    document.write("U");
+}else if (ASCII === 86 ){
+    document.write("V");
+}else if (ASCII === 87 ){
+    document.write("W");
+}else if (ASCII === 88 ){
+    document.write("X");
+}else if (ASCII === 89 ){
+    document.write("Y");
+}else if (ASCII === 90 ){
+    document.write("Z");
+}else if (ASCII === 97 ){
+    document.write("a");
+}else if (ASCII === 98 ){
+    document.write("b");
+}else if (ASCII === 99 ){
+    document.write("c");
+}else if (ASCII === 100 ){
+    document.write("d");
+}else if (ASCII === 101 ){
+    document.write("e");
+}else if (ASCII === 102 ){
+    document.write("f");
+}else if (ASCII === 103 ){
+    document.write("g");
+}else if (ASCII === 104 ){
+    document.write("h");
+}else if (ASCII === 105 ){
+    document.write("i");
+}else if (ASCII === 106 ){
+    document.write("j");
+}else if (ASCII === 107 ){
+    document.write("k");
+}else if (ASCII === 108 ){
+    document.write("l");
+}else if (ASCII === 109 ){
+    document.write("m");
+}else if (ASCII === 110 ){
+    document.write("n");
+}else if (ASCII === 111 ){
+    document.write("o");
+}else if (ASCII === 112 ){
+    document.write("p");
+}else if (ASCII === 113 ){
+    document.write("q");
+}else if (ASCII === 114 ){
+    document.write("r");
+}else if (ASCII === 115 ){
+    document.write("s");
+}else if (ASCII === 116 ){
+    document.write("t");
+}else if (ASCII === 117 ){
+    document.write("u");
+}else if (ASCII === 118 ){
+    document.write("v");
+}else if (ASCII === 119 ){
+    document.write("w");
+}else if (ASCII === 120 ){
+    document.write("x");
+}else if (ASCII === 121 ){
+    document.write("y");
+}else if (ASCII === 122 ){
+    document.write("z");
+}else if (ASCII === 48 ){
+    document.write("0");
+}else if (ASCII === 49 ){
+    document.write("1");
+}else if (ASCII === 50 ){
+    document.write("2");
+}else if (ASCII === 51 ){
+    document.write("3");
+}else if (ASCII === 52 ){
+    document.write("4");
+}else if (ASCII === 53 ){
+    document.write("5");
+}else if (ASCII === 54 ){
+    document.write("6");
+}else if (ASCII === 55 ){
+    document.write("7");
+}else if (ASCII === 56 ){
+    document.write("8");
+}else if (ASCII === 57 ){
+    document.write("9");
+}
+
+
+
+
+
+
+
+
+
+
+
